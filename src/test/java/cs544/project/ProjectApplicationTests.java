9293dc0ab07e62703def7760b03c9e88212f030f
mokezeru <mokemz24@gmail.com>
2019-02-13 16:54:50 -0600

    adding test

--- src/test/java/cs544/project/ProjectApplicationTests.java
+++ src/test/java/cs544/project/ProjectApplicationTests.java
@@ -1,9 +1,12 @@
 package cs544.project;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.AssertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,7 @@ public class ProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
